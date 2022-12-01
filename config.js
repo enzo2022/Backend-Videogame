de4d1f4a134bd7b7881d7f07a20a3d172725d95a
enzo2022 <enzo142014@hotmail.com>
2022-12-01 14:36:04 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+
+
+const PORT = process.env.PORT || 3000
+const DB_USER =  " postgres"
+ const DB_PASSWORD = "40990161em"
+ const DB_HOST=  'localhost'
+ const DATABASE = 'videogames'
+
+
+ module.exports ={
+    PORT,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DATABASE
+ }
+
