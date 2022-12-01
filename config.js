40efe2eaa234c0fc69c207bb167604d190376be6
enzo2022 <enzo142014@hotmail.com>
2022-12-01 15:20:31 -0300

    okkk

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 
 
-const PORT = process.env.PORT || 3000
-const DB_USER =  " postgres"
- const DB_PASSWORD = "40990161em"
- const DB_HOST=  'localhost'
- const DATABASE = 'videogames'
+const {PORT} = process.env.PORT || 3000
+const {DB_USER} = process.env.DB_USER ||' postgres'
+ const DB_PASSWORD =process.env.DB_PASSWORD || `40990161em`
+ const DB_HOST = process.env.DB_HOST || 'localhost'
+ const DATABASE = process.env.DATABASE || 'videogames'
 
 
  module.exports ={
     PORT,
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DATABASE
+    DB_USER
  }
 
+ Videogame.belongsToMany(Genre,{through: "Videogame_Genre"})
+ Genre.belongsToMany(Videogame,{through: "Videogame_Genre"})
--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {PORT} = process.env
+const PORT = process.env.PORT || 3000
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
